d2449a1c3a35d1657c6a9d806daf0a88da380455
griseldans <griseldaneysasaadiya@gmail.com>
2023-05-07 09:41:04 +0700

    revisi path pelatih, pemain

--- mng_kelola_tim/urls.py
+++ mng_kelola_tim/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from mng_kelola_tim.views import daftar_tim, detail_tim
+from mng_kelola_tim.views import daftar_tim, detail_tim, daftar_pemain, daftar_pelatih
 app_name = "mng_kelola_tim"
 
 urlpatterns = [
     path('daftar-tim/', daftar_tim, name='daftar-tim'),
     path('detail-tim/', detail_tim, name='detail-tim'),
-    path('daftar_tim/', daftar_tim, name='dftar-tim'),
-    path('daftar-pelatih/', detail_tim, name='daftar_pelatih'),
+    path('daftar_tim/', daftar_pemain, name='daftar-pemain'),
+    path('daftar-pelatih/', daftar_pelatih, name='daftar_pelatih'),
 ]
