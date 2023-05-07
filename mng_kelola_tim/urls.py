3889ce48ceb88548cdf27303b3d5ab8e51cf7e72
griseldans <griseldaneysasaadiya@gmail.com>
2023-05-07 09:02:22 +0700

    akses html

--- mng_kelola_tim/urls.py
+++ mng_kelola_tim/urls.py
@@ -4,5 +4,7 @@ app_name = "mng_kelola_tim"
 
 urlpatterns = [
     path('daftar-tim/', daftar_tim, name='daftar-tim'),
-    path('detail-tim/', detail_tim, name='detail-tim')
+    path('detail-tim/', detail_tim, name='detail-tim'),
+    path('daftar_tim/', daftar_tim, name='dftar-tim'),
+    path('daftar-pelatih/', detail_tim, name='daftar_pelatih'),
 ]
