62a56a227146e066977586cee9639ccb826c7dc1
griseldans <griseldaneysasaadiya@gmail.com>
2023-05-07 17:05:18 +0700

    isi app mulai_rapat benar

--- mulai_rapat/views.py
+++ mulai_rapat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def daftar_mulai_rapat(request):
+    context = {}
+    return render(request, 'daftar_mulai_rapat.html', context)
+
+def daftar_isi_rapat(request):
+    context = {}
+    return render(request, 'daftar_isi_rapat.html', context)
+
+
